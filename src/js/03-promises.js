bf2b6b0cb26ec7df74377ccae48bb45825943d72
OleksandrDukach <umnick36@gmail.com>
2023-07-27 10:45:04 +0300

    q

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,34 @@
+const newForm = document.querySelector('.form');
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+  return promise;
+}
+
+newForm.addEventListener('submit', createNewPromise);
+
+function createNewPromise(evt) {
+  evt.preventDefault();
+  const delay = Number(evt.target[0].value);
+  const step = Number(evt.target[1].value);
+  const amount = Number(evt.target[2].value);
+
+  for (let i = 0; i <= amount; i += 1) {
+    createPromise(2, 1500)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
   }
 }
