9229a29031884e8e1719b9af19474568fd4f9fab
OleksandrDukach <umnick36@gmail.com>
2023-07-27 10:29:06 +0300

    q

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const refs = {
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+  bodyElem: document.querySelector('body'),
+  intervalColorChange: null,
+};
 
+refs.startBtn.addEventListener('click', startChangeColor);
+refs.stopBtn.addEventListener('click', stopChangeColor);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function startChangeColor(event) {
+  refs.intervalColorChange = setInterval(() => {
+    refs.bodyElem.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  refs.startBtn.disabled = true;
+}
+
+function stopChangeColor(event) {
+  clearInterval(refs.intervalColorChange);
+  refs.startBtn.disabled = false;
+  refs.bodyElem.style.backgroundColor = '#fff';
+}
